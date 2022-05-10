4db23aefebbf5c3aae78c132bc627414409c3e57
Shakib202 <shakib2020360@gmail.com>
2022-05-11 04:48:29 +0600

    'commit'

--- _server_deploy.js
+++ _server_deploy.js
@@ -10,4 +10,21 @@
 * FOR EACH PROJECT ONE TIME
 * -----------------------------
 * 1. heroku create
+* 2. make sure you: git add . / git commit -m 'all done' / git push
+* 3. git push heroku main
+* 4. Go to Heroku Dashboard > Current Project > Settings > Reveal Config Vars
+* 5. copy past config vars from your .env file
+* 6. Make sure you have whitelisted all ip address to access mongodb
+* -----------------------------
+* UPDATE SERVER WITH NEW CHANGES
+* -----------------------------
+* 1. Make changes
+* 2. make sure you: git add . / git commit -m 'all done' / git push
+* 3. git push heroku main
+* -----------------------------
+* CONNECT SERVER WITH CLIENT
+* -----------------------------
+* 1. replace localhost by heroku link
+* 2. npm run build
+* 3. firebase deploy
 */ 
